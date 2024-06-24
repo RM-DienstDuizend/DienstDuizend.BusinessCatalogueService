810cb4f7f01ec76e40c6176e7ff63331a76711c9
Robin Mager <62018777+BlackBearFTW@users.noreply.github.com>
2024-06-24 09:28:18 +0200

    Unit tests

---
+++ tests/DienstDuizend.BusinessCatalogueService.UnitTests/Utils/MarkdownSanitizerTest.cs
@@ -0,0 +1,16 @@
+﻿using DienstDuizend.BusinessCatalogueService.Infrastructure.Utilities;
+
+namespace DienstDuizend.BusinessCatalogueService.UnitTests.Utils;
+
+public class MarkdownSanitizerTest
+{
+    [Theory]
+    [InlineData("[hello](javascript:prompt(document.cookie))", "hello")]
+    [InlineData("<script>alert(1);</script>", "")]
+    [InlineData("<img src/onerror=prompt(8)>", "&lt;img src/onerror=prompt(8)&gt;")]
+    public void Should_Clean_MaliciousContents(string raw, string after)
+    {
+        var result = MarkdownSanitizer.Sanitize(raw);
+        result.Should().BeEquivalentTo(after);
+    }
+}
